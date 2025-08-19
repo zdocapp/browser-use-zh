8ee41dbb5f3617346016dc4ebd792d240f9d0bbb
Magnus Müller <67061560+MagMueller@users.noreply.github.com>
2025-08-18 18:05:41 -0700

    Simple example

--- examples/simple.py
+++ examples/simple.py
@@ -2,28 +2,26 @@ import asyncio
 import os
 import sys
 
-from browser_use.llm.openai.chat import ChatOpenAI
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-from browser_use import Agent
-
-# Initialize the model
-llm = ChatOpenAI(
-	model='gpt-5-mini',
-)
-
-
-task = 'Go and find the founders of browser-use'
-agent = Agent(task=task, llm=llm)
+from browser_use import Agent, ChatOpenAI
 
 
 async def main():
+	# Choose your model
+	llm = ChatOpenAI(model='gpt-5-mini')
+
+	# Define your task
+	task = 'Go and find the founders of browser-use'
+
+	# Create the agent
+	agent = Agent(task=task, llm=llm)
+
+	# Start
 	await agent.run()
 
 
